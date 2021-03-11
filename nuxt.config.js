9d1d46e7a85420d972fa02dec523550ad2564910
ed85w <ed.walker@hotmail.co.uk>
2021-03-11 12:27:15 +0000

    router base amended for domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/ids-industrial/'
+    base: ''
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
