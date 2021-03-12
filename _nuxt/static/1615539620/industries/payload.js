a090a6a61c60a9814f66b7877f7890081711257a
ed85w <ed.walker@hotmail.co.uk>
2021-03-12 09:00:46 +0000

    8733f5a

---
+++ _nuxt/static/1615539620/industries/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/industries", {data:[{}],fetch:[],mutations:void 0});
---
+++ js/cookie-consent.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "rgb(42, 75, 155)",
+        "padding": "10px"
+      },
+      "button": {
+        "background": "transparent",
+        "text": "#f1d600",
+        "border": "#f1d600"
+      }
+    }
+  })
+});
